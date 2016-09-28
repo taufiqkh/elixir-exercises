6dfe928ec4c3f1b216378d5614e0ad927e673a29
Taufiq Hoven <taufiq.hoven@gmail.com>
2016-09-28 16:51:09 +1000

    Strings and Binaries section completed

---
+++ lib/control_flow.ex
@@ -0,0 +1,4 @@
+defmodule ControlFlow do
+  @moduledoc false
+  
+end
--- lib/strings_and_binaries.ex
+++ lib/strings_and_binaries.ex
@@ -1,6 +1,43 @@
 defmodule StringsAndBinaries do
   @moduledoc false
 
+  @upperOffset ?A - ?a
+  def _capitalise(<<?. :: utf8, <<?\s :: utf8, <<head :: utf8, rest :: binary >> >> >>) when head >= ?A and head <= ?z do
+    newHead = if head >= ?a and head <= ?z do
+      head + @upperOffset
+    else
+      head
+    end
+    <<?., <<?\s, <<newHead :: utf8, _capitalise(rest) :: binary >> >> >>
+  end
+  def _capitalise(<<>>), do: <<>>
+  def _capitalise(<<head :: utf8, rest :: binary>>) when head >= ?A and head <= ?Z do
+    <<head - @upperOffset, _capitalise(rest) :: binary>>
+  end
+  def _capitalise(<<head :: utf8, rest :: binary>>), do: <<head, _capitalise(rest) :: binary>>
+
+  def capitalize_sentences(<<head :: utf8, rest :: binary>>) when head >= ?a and head <= ?z do
+    <<head + @upperOffset, _capitalise(rest) :: binary >>
+  end
+  def capitalize_sentences(str), do: _capitalise(str)
+
+  def _read_by_line(_file, :eof), do: []
+  def _read_by_line(_file, {:error, reason}) do
+    IO.puts reason
+    []
+  end
+  def _read_by_line(file, data) do
+    [id, ship_to, net_amount] = String.split(String.trim(data), ",")
+    [[id: id, ship_to: ship_to, net_amount: String.to_float(net_amount)] | _read_by_line(file, IO.read(file, :line))]
+  end
+  def _read(file) do
+    IO.read(file, :line)
+    _read_by_line(file, IO.read(file, :line))
+  end
+  def file_read() do
+    File.open("data.txt", [:read], &_read/1)
+  end
+
   def printable?([]), do: true
   def printable?([head | tail]), do: (head >= ?\s and head <= ?~ and printable?(tail))
 
@@ -26,6 +63,7 @@ defmodule StringsAndBinaries do
     longest = _longest(strs, 0)
     _center(strs, longest)
   end
+
   defp _as_number([], num), do: num
   defp _as_number([head | tail], num), do: _as_number(tail, num * 10 + head - ?0)
   defp _as_number(str), do: _as_number(str, 0)
@@ -41,6 +79,7 @@ defmodule StringsAndBinaries do
   defp _calculate([head | tail], num) when head == ?/ do
     num / _as_number(tail)
   end
+
   defp _calculate([head | tail], num) do
     _calculate(tail, num * 10 + head - ?0)
   end
