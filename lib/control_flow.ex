da0372f962f230c08fb46fed3026877b97db7a42
Taufiq Hoven <taufiq.hoven@gmail.com>
2016-10-03 00:11:16 +1000

    Control flow fizzbuzz completed

--- lib/control_flow.ex
+++ lib/control_flow.ex
@@ -1,4 +1,14 @@
 defmodule ControlFlow do
   @moduledoc false
-  
+
+  def ok!({:ok, data}), do: data
+  def ok!(param), do: raise "#{inspect param}"
+  def fizzbuzz(n) do
+    case {rem(n, 3), rem(n, 5)} do
+      {0, 0} -> "FizzBuzz"
+      {0, _} -> "Fizz"
+      {_, 0} -> "Buzz"
+      _ -> n
+    end
+  end
 end
