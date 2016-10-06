2bc7c0d0f310191b9017726be5f5f605fdf2ac04
Taufiq Hoven <taufiq.hoven@gmail.com>
2016-10-06 23:05:30 +1000

    Exercises 3,4,5 for chapter 16

--- lib/chapter_16/stack_server.ex
+++ lib/chapter_16/stack_server.ex
@@ -3,11 +3,31 @@ defmodule StackServer do
 
   use GenServer
 
-  def handle_call(:pop, _from_pid, [head | rest]) do
-    {:reply, head, rest}
+  def start_link(max_len) do
+    GenServer.start_link(__MODULE__, {max_len, []}, name: :stack)
   end
 
-  def handle_cast({:push, delta}, list) do
-    {:noreply, [delta | list]}
+  def pop do
+    GenServer.call(:stack, :pop)
+  end
+
+  def push(elem) do
+    GenServer.cast(:stack, {:push, elem})
+  end
+
+  def handle_call(:pop, _from_pid, {max_len, [head | rest]}) do
+    {:reply, head, {max_len, rest}}
+  end
+
+  def handle_cast({:push, delta}, {max_len, list}) do
+    if Enum.count(list) == max_len do
+      System.halt(1)
+    else
+      {:noreply, {max_len, [delta | list]}}
+    end
+  end
+
+  def terminate(reason, state) do
+    IO.puts "Terminating because #{inspect reason}, state: #{inspect state}"
   end
 end
