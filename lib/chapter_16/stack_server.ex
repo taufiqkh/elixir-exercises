a2a47e5a8d2fa6bd5e5a3edc90100810acdda879
Taufiq Hoven <taufiq.hoven@gmail.com>
2016-10-06 18:37:53 +1000

    Added chapter 16 exercise 2, OTP Servers

--- lib/chapter_16/stack_server.ex
+++ lib/chapter_16/stack_server.ex
@@ -6,4 +6,8 @@ defmodule StackServer do
   def handle_call(:pop, _from_pid, [head | rest]) do
     {:reply, head, rest}
   end
+
+  def handle_cast({:push, delta}, list) do
+    {:noreply, [delta | list]}
+  end
 end
